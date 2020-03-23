65728daa58b524387ea7fdb8f0a51c051cf959ac
rikoli <939958083@qq.com>
2020-03-23 12:44:12 +0800

    add NCC metric

--- codes/py_codes/switch_matcher.py
+++ codes/py_codes/switch_matcher.py
@@ -1,5 +1,6 @@
 import cv2.cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt # for test
 
 class SwitchMatcher:
     '''
@@ -19,16 +20,15 @@ class SwitchMatcher:
             np.array, template image
         '''
         template = cv.imread(template_path, 1)
-        template = cv.GaussianBlur(template, (3,3), 5)
         return template
     
-    def _get_loss(self, img):
+    def _get_loss_l2(self, img):
         '''
-        Compute loss between given image and template.\n
+        Compute L2-loss between given image and template.\n
         @img:
             np.array, input image
         @return:
-            float, loss between given image and template
+            float, L2-loss between given image and template
         '''
         img = cv.GaussianBlur(img, (3,3), 5) # smoothing
         img = cv.resize(img, (self.template.shape[1], self.template.shape[0])) # align in size
@@ -47,30 +47,39 @@ class SwitchMatcher:
 
         return loss
 
-    def _get_variance_smoothness(self, roi):
-        '''
-        Get variance smoothness of given ROI.\n
-        @roi:
-            np.array, ROI which you want to compute the pixel variance
-        @return:
-            float, pixel variance of given ROI
-        '''
-        rows, cols, _ = roi.shape
-        roi = cv.GaussianBlur(roi, (3,3), 1.5)
-        hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-        h, s, v = cv.split(hsv)
-        mean = np.array([np.mean(h, dtype=np.float64), np.mean(s, dtype=np.float64), np.mean(v, dtype=np.float64)]).reshape((1,-1))
-        losses = []
-        for r in range(rows):
-            for c in range(cols):
-                value = hsv[r, c].reshape((1,-1))
-                losses.append(np.linalg.norm(value-mean))
-        return np.var(np.array(losses, np.float64))
+    def _get_entropy_single(self, roi):
+        hist = cv.calcHist([roi], [0], None, [256], (0,256))
+        hist = [h[0] for h in hist.tolist()] # distribution
+        prob = [h / sum(hist) for h in hist] # probability
+        entropy = sum([-p * np.log2(p) for p in prob if p != 0]) # entropy
+        return entropy
 
-    def _get_entropy_smoothness(self, img):
-        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        hist = cv.calcHist([img], [0], None, [256], (0,256))
-        return hist
+    def _get_mean_single(self, roi):
+        return np.mean(roi)
+
+    def _get_variance_single(self, roi):
+        return np.var(roi)
+
+    def _get_smoothness_single(self, roi):
+        return 1 / (1 + self._get_variance_single(roi))
+
+    def _get_ncc_similarity_single(self, roi):
+        # smoothing
+        template = cv.GaussianBlur(self.template, (3,3), 1)
+
+        # align in shape
+        roi = cv.resize(roi, (template.shape[1], template.shape[0]))
+
+        # mean reduce
+        template = template - np.mean(template)
+        roi = roi - np.mean(roi)
+
+        # compute relation factor
+        numerator = np.sum(np.multiply(template, roi))
+        denominator = np.sqrt(np.sum(np.square(template))) * np.sqrt(np.sum(np.square(roi)))
+
+        # return numerator / denominator
+        return denominator / numerator # loss version
 
     def match(self):
         '''
@@ -82,11 +91,18 @@ class SwitchMatcher:
             return []
         loss_maps = []
         for i, roi in enumerate(self.proposals):
-            loss_maps.append({'loss': self._get_loss(roi), 'id': i})
+            # loss_maps.append({'loss': self._get_loss_l2(roi), 'id': i})
+            loss_maps.append({'loss': self._get_ncc_similarity_single(roi), 'id': i})
         sorted_loss_maps = sorted(loss_maps, key=lambda loss_map: loss_map['loss'])
         return sorted_loss_maps
         
 
 
 if __name__ == "__main__":
-    pass
+    img = cv.imread('../../images/original/2.26_template.jpg', 0)
+    img = cv.GaussianBlur(img, (3,3), 1.0)
+    matcher = SwitchMatcher('test', [])
+    out = matcher._get_entropy_single(img)
+    print(out)
+    # plt.hist(hist, 256, (0,256))
+    # plt.show()
